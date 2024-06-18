e5614d54cd6ddc757a1ee76c00549ad342de773e
André Costa <toscmen.com@gmail.com>
2024-06-18 18:40:30 +0200

    startup project

---
+++ Core/Src/main.c
@@ -0,0 +1,213 @@
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+#include "main.h"
+#include "stm32l4xx_hal_uart.h"
+#include <stdint.h>
+#include <stdio.h>
+
+SPI_HandleTypeDef w5500_spi;
+
+UART_HandleTypeDef console_uart;
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_SPI1_Init(void);
+int __io_putchar(int byte)
+{
+	return HAL_UART_Transmit(&console_uart, (uint8_t *)&byte, 1, 100);
+}
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+	HAL_Init();
+
+	SystemClock_Config();
+
+	MX_GPIO_Init();
+	MX_USART2_UART_Init();
+	MX_SPI1_Init();
+	printf("Hello World!\r\n");
+
+	while (1) {
+	}
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+
+	/** Configure the main internal regulator output voltage
+  */
+	if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) !=
+	    HAL_OK) {
+		Error_Handler();
+	}
+
+	/** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+	RCC_OscInitStruct.PLL.PLLM = 1;
+	RCC_OscInitStruct.PLL.PLLN = 10;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
+	RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+	RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
+
+	/** Initializes the CPU, AHB and APB buses clocks
+  */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK |
+				      RCC_CLOCKTYPE_SYSCLK |
+				      RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) !=
+	    HAL_OK) {
+		Error_Handler();
+	}
+}
+
+/**
+  * @brief SPI1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI1_Init(void)
+{
+	/* SPI1 parameter configuration*/
+	w5500_spi.Instance = SPI1;
+	w5500_spi.Init.Mode = SPI_MODE_MASTER;
+	w5500_spi.Init.Direction = SPI_DIRECTION_2LINES;
+	w5500_spi.Init.DataSize = SPI_DATASIZE_8BIT;
+	w5500_spi.Init.CLKPolarity = SPI_POLARITY_LOW;
+	w5500_spi.Init.CLKPhase = SPI_PHASE_1EDGE;
+	w5500_spi.Init.NSS = SPI_NSS_SOFT;
+	w5500_spi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_4;
+	w5500_spi.Init.FirstBit = SPI_FIRSTBIT_MSB;
+	w5500_spi.Init.TIMode = SPI_TIMODE_DISABLE;
+	w5500_spi.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+	w5500_spi.Init.CRCPolynomial = 7;
+	w5500_spi.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
+	w5500_spi.Init.NSSPMode = SPI_NSS_PULSE_DISABLE;
+	if (HAL_SPI_Init(&w5500_spi) != HAL_OK) {
+		Error_Handler();
+	}
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+	console_uart.Instance = USART2;
+	console_uart.Init.BaudRate = 115200;
+	console_uart.Init.WordLength = UART_WORDLENGTH_8B;
+	console_uart.Init.StopBits = UART_STOPBITS_1;
+	console_uart.Init.Parity = UART_PARITY_NONE;
+	console_uart.Init.Mode = UART_MODE_TX_RX;
+	console_uart.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	console_uart.Init.OverSampling = UART_OVERSAMPLING_16;
+	console_uart.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+	console_uart.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+	if (HAL_UART_Init(&console_uart) != HAL_OK) {
+		Error_Handler();
+	}
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+
+	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	__HAL_RCC_GPIOH_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
+
+	/*Configure GPIO pin : B1_Pin */
+	GPIO_InitStruct.Pin = B1_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+	/*Configure GPIO pin : PB6 */
+	GPIO_InitStruct.Pin = GPIO_PIN_6;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1) {
+	}
+	/* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+	/* USER CODE BEGIN 6 */
+	/* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+	/* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
